20de3d7302a474cebd0c0e0ccf483be710a7fd92
230045 Zagozda Konrad <230045@edu.p.lodz.pl>
2020-10-19 20:27:48 +0200

    delete fillBoardUtils inner class, make isFull member of SudokuBoard class

--- src/main/java/komponentowe/zadanie2/SudokuBoard.java
+++ src/main/java/komponentowe/zadanie2/SudokuBoard.java
@@ -17,23 +17,21 @@ public class SudokuBoard {
         //todo
     }
 
-    public int get(int rowIndex, int columnIndex){ //
-        if ( columnIndex < 9 && rowIndex < 9){
+    public int get(int rowIndex, int columnIndex) { //
+        if (columnIndex < 9 && rowIndex < 9) {
             return sudokuBoard[rowIndex][columnIndex];
         } else {
             return 0;
         }
     }
 
-    private static class fillBoardUtils {
-        static boolean isFull(int[][] sudokuBoard) {
-            for (int row = 0; row < 9; row++) {
-                for (int column = 0; column < 9; column++) {
-                    if (sudokuBoard[row][column] == 0) return false;
-                }
+    private boolean isFull(int[][] sudokuBoard) {
+        for (int row = 0; row < 9; row++) {
+            for (int column = 0; column < 9; column++) {
+                if (sudokuBoard[row][column] == 0) return false;
             }
-            return true;
         }
+        return true;
     }
 
 }
