8426847b06da0c2c3d4462c491450ad1f4ce429a
230045 Zagozda Konrad <230045@edu.p.lodz.pl>
2020-10-19 14:37:43 +0200

    initial commit

---
+++ src/main/java/pl/first/firstjava/App.java
@@ -0,0 +1,13 @@
+package pl.first.firstjava;
+
+/**
+ * Klasa główna z metodą main.
+ * @author Marcin Kwapisz
+ */
+public class App {
+
+    public static void main(final String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.greet(args[0]));
+    }
+}
