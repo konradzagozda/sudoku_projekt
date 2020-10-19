6335190905fd73678545240f4653be87252f9e65
Wojciech Mikolajczak <mikolajczak.wojtek@gmail.com>
2020-10-19 17:49:51 +0200

    update fillBoard and showBoard methods

--- src/main/java/komponentowe/zadanie2/SudokuBoard.java
+++ src/main/java/komponentowe/zadanie2/SudokuBoard.java
@@ -1,5 +1,8 @@
 package komponentowe.zadanie2;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class SudokuBoard {
 
     private int[][] sudokuBoard;
@@ -10,16 +13,24 @@ public class SudokuBoard {
 
     public void fillBoard() {
         System.out.println("Backtracking");
+
+        Random rand = new Random();
+        for (int i = 0; i <= 8; i++) {
+            for (int j = 0; j <= 8; j++) {
+                sudokuBoard[i][j] = (int) rand.nextInt(9) + 1;
+            }
+        }
     }
 
     public void showBoard() {
         System.out.println("Showing board...");
+        System.out.println(Arrays.deepToString(sudokuBoard));
     }
 
     //getters
 
-
     public int[][] getSudokuBoard() {
         return sudokuBoard;
     }
+
 }
--- src/test/java/komponentowe/zadanie2/SudokuBoardTest.java
+++ src/test/java/komponentowe/zadanie2/SudokuBoardTest.java
@@ -1,23 +1,29 @@
 package komponentowe.zadanie2;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SudokuBoardTest {
 
-    SudokuBoard newBoard;
+    SudokuBoard testBoard;
 
     @BeforeEach
     void setupBoard() {
-        newBoard = new SudokuBoard();
+        testBoard = new SudokuBoard();
     }
 
     @Test
     void fillBoardTest() {
-        newBoard.fillBoard();
-        assertEquals(45, newBoard.getSudokuBoard()[1][1]);
+        testBoard.fillBoard();
+        testBoard.showBoard();
+        System.out.println(testBoard.getSudokuBoard()[0][8]);
+        for (int i = 0; i <= 8; i++) {
+            for (int j = 0; j <= 8; j++) {
+                int rowSum =+ testBoard.getSudokuBoard()[i][j];
+                assertEquals(45, rowSum);
+            }
+        }
     }
 }
