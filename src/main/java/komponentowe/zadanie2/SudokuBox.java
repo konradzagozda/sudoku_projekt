ba950c815a28440156ff0aae7473b89c48360c01
229961 Mikolajczak Wojciech <229961@edu.p.lodz.pl>
2020-11-23 13:04:04 +0100

    delete unused import declarations

--- src/main/java/komponentowe/zadanie2/SudokuBox.java
+++ src/main/java/komponentowe/zadanie2/SudokuBox.java
@@ -1,6 +1,5 @@
 package komponentowe.zadanie2;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class SudokuBox extends SudokuStructure {
--- src/test/java/komponentowe/zadanie2/SudokuStructureTest.java
+++ src/test/java/komponentowe/zadanie2/SudokuStructureTest.java
@@ -3,7 +3,6 @@ package komponentowe.zadanie2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
